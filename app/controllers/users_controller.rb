f2f40f382be8ee4dd237572540e3e33f0bc6a11d
bmolina-nyc <bruce.molina.81@gmail.com>
2017-02-26 16:58:40 -0500

    still updating signup - added sessions, edited migrations

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,24 @@
 class UsersController < ApplicationController
 
+  # create a user - the form is directly on the login page 
   def create
-    user = User.new(user_params).save
+    user = User.new(user_params)
+
+    if user.save
+      redirect_to 'root_path'
+    else
+      redirect to  'root_path'
+    end
   end
  
+
+
+
+
   private
  
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:username, :password, :password_confirmation)
   end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  
+
+  resources :sessions
+  resources :users
+  # the root of the app
+  root 'welcome#homepage'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

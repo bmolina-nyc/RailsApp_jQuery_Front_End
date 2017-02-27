9b001078fb806832bf11d9c74e6225a7e2e206dd
bmolina-nyc <bruce.molina.81@gmail.com>
2017-02-26 19:59:46 -0500

    updated css on login page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,9 +5,9 @@ class UsersController < ApplicationController
     user = User.new(user_params)
 
     if user.save
-      redirect_to 'root_path'
+      redirect_to root_path
     else
-      redirect to  'root_path'
+      redirect_to  root_path
     end
   end
  
